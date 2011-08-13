7f9ad06b564becb5c0aa232245bc1b0b4587e61b
森井ゴンザレス <morygonzalez@gmail.com>
2011-08-13 15:02:57 +0900

    tagをtagsにぶっ込むところまで

--- lib/tag_loader.rb
+++ lib/tag_loader.rb
@@ -2,25 +2,50 @@
 #-*- encoding: UTF-8 -*-
 
 require "yaml"
+require "dm-core"
+require "dm-tags"
 
 YAML::ENGINE.yamler= 'syck'
+
+class Tags
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String, :length => 255
+end
+
+DataMapper::Model.raise_on_save_failure = true
 
 class TagLoader
   def initialize
     @yaml = "p_blog_log.yml"
   end
-  
+
   def load_yaml
     YAML.load_file(File.dirname(__FILE__) + "/../#{@yaml}")
   end
-  
+
   def list_tags
     tags = []
     load_yaml.each do |log|
       log["tag"].split(",").each do |t|
-        tags << t
+        tags << t.downcase.strip
       end
     end
     tags.uniq
+  end
+
+  def save_tags
+    list_tags.each do |tag|
+      begin
+        @tag = Tag.create(
+          :id => nil,
+          :name => tag
+        )
+        @tag.save
+      rescue DataMapper::SaveFailureError
+        return tag["name"]
+      end
+    end
   end
 end
--- spec/tag_loader_spec.rb
+++ spec/tag_loader_spec.rb
@@ -11,14 +11,32 @@ describe TagLoader do
       subject.load_yaml.should be_true
     end
 
-    it { subject.list_tags.should be_kind_of(Array) }
+    it "タグの一覧をYAMLから読み込む" do
+      subject.list_tags.should be_kind_of(Array)
+    end
+
+    it "空白がトリムされてる" do
+      subject.list_tags[rand(subject.list_tags.size)].should_not match(/^\s.+/)
+      subject.list_tags[rand(subject.list_tags.size)].should_not match(/.+\s$/)
+    end
+    
+    it "アルファベットは小文字になってる" do
+      @target = subject.list_tags[rand(subject.list_tags.size)]
+      @target.should match(/[a-z]+/) if @target =~ /[a-zA-Z]+/
+    end
   end
 
   it "tagsテーブルに取得したtagをそれぞれ保存する" do
-    pending
+    pending "やんごとなき理由"
+    subject.save_tags.should be_true
   end
 
   it "タグのある記事とタグを関連づける" do
     pending
+  end
+
+  after(:all) do
+    Tag.all.destroy
+    Tag.repository.adapter.execute('update sqlite_sequence set seq=0 where name="tags";')
   end
 end
